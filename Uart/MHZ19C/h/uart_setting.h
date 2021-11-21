5c324fc1fcf349f6c35638136c37dbbb2b7a4b7b
link2387 <link2387@mail.ru>
2021-11-21 18:11:05 +0700

    add

---
+++ Uart/MHZ19C/h/uart_setting.h
@@ -0,0 +1,16 @@
+/*
+ * uart_setting.h
+ *
+ *  Created on: 14 нояб. 2021 г.
+ *      Author: root
+ */
+
+#ifndef UART_SETTING_H_
+#define UART_SETTING_H_
+
+void set_speed(int fd, int speed);
+bool set_Parity(int fd,int databits,int stopbits,int parity);
+int OpenDev(char *Dev);
+int defaultConfUart(char *device);
+
+#endif /* UART_SETTING_H_ */
