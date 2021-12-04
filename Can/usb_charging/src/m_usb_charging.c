73d0fe995198bd3d76f579aa72c6db138393086a
link2387 <link2387@mail.ru>
2021-12-05 00:06:53 +0700

    add usb_charging

---
+++ Can/usb_charging/src/m_usb_charging.c
@@ -0,0 +1,60 @@
+/*
+ ============================================================================
+ Name        : Mqtt_Mh-Z19C.c
+ Author      : lns
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <apds9300.h>
+#include <common.h>
+
+void* read_sensor(void *param) {
+	int *value_array = NULL;
+	usr_cfg_t ucfg;
+	devSensorFunc_t dSf;
+
+	getSensorFncAPDS9300(&dSf);
+	init(param, &ucfg, &dSf, CANS);
+	value_array = malloc((ucfg.mqtt_read->param_size) * sizeof(int));
+	while (1) {
+		dSf.getMeasurement(value_array, ucfg.mqtt_read);
+		mqttResultPubInt(&ucfg, value_array);
+		usleep(100);
+	}
+	pthread_exit(SUCCESS);
+	return SUCCESS;
+}
+
+void terminate(int param) {
+	exit(FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+	int id;
+	signal(SIGTERM, terminate);
+	signal(SIGINT, terminate);
+
+	if (argc < 2) {
+		printf("You need input id aplication.\n");
+		exit(1);
+	}
+	id = atoi(argv[1]);
+	printf("Start aplication id %d \n", id);
+
+	pthread_t thr;
+	int status;
+	status = pthread_create(&thr, NULL, read_sensor, &id);
+	if (status != 0) {
+		printf("main error: can't create thread, status = %d\n", status);
+		exit(ERROR_CREATE_THREAD);
+	}
+
+	status = pthread_join(thr, 0);
+	if (status != SUCCESS) {
+		printf("main error: can't join thread, status = %d\n", status);
+		exit(ERROR_JOIN_THREAD);
+	}
+	exit(SUCCESS);
+}
